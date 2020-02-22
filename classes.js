67a67699bb69f400981bb94ce5a66dc803c07f6c
tom-rose-codeman <tomrlrose@gmail.com>
2020-02-22 17:20:02 +1300

    call three funct

--- classes.js
+++ classes.js
@@ -6,7 +6,8 @@ document.addEventListener('DOMContentLoaded', start)
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
    one()
-   two() 
+   two()
+   three() 
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
